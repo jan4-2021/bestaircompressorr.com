50c67a00bfb4bfc0e6563600afd364b4f82b74fc
jaydeep <jaydeep_gajera13051993@yahoo.com>
2021-01-04 20:30:49 +0530

    first commit

---
+++ wp-login.php
@@ -0,0 +1,112 @@
+<!DOCTYPE html>
+	<!--[if IE 8]>
+		<html xmlns="http://www.w3.org/1999/xhtml" class="ie8" lang="en-US">
+	<![endif]-->
+	<!--[if !(IE 8) ]><!-->
+		<html xmlns="http://www.w3.org/1999/xhtml" lang="en-US">
+	<!--<![endif]-->
+	<head>
+
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+	<title>Best Air Compressors 2016 &#8211; Reviews &amp; Top Pick &lsaquo; Log In</title>
+	<link rel="dns-prefetch" href="http://s.w.org/"/>
+<script type="text/javascript" src="http://bestaircompressorr.com/wp-admin/load-scripts.php?c=0&amp;load%5B%5D=jquery-core,jquery-migrate&amp;ver=5aa486d7402e19f267d6414b8f60d710"></script>
+<link rel="stylesheet" href="http://bestaircompressorr.com/wp-admin/load-styles.php?c=0&amp;dir=ltr&amp;load%5B%5D=dashicons,buttons,forms,l10n,login&amp;ver=5aa486d7402e19f267d6414b8f60d710" type="text/css" media="all"/>
+<script type="text/javascript">
+(function(url){
+	if(/(?:Chrome\/26\.0\.1410\.63 Safari\/537\.31|WordfenceTestMonBot)/.test(navigator.userAgent)){ return; }
+	var addEvent = function(evt, handler) {
+		if (window.addEventListener) {
+			document.addEventListener(evt, handler, false);
+		} else if (window.attachEvent) {
+			document.attachEvent('on' + evt, handler);
+		}
+	};
+	var removeEvent = function(evt, handler) {
+		if (window.removeEventListener) {
+			document.removeEventListener(evt, handler, false);
+		} else if (window.detachEvent) {
+			document.detachEvent('on' + evt, handler);
+		}
+	};
+	var evts = 'contextmenu dblclick drag dragend dragenter dragleave dragover dragstart drop keydown keypress keyup mousedown mousemove mouseout mouseover mouseup mousewheel scroll'.split(' ');
+	var logHuman = function() {
+		var wfscr = document.createElement('script');
+		wfscr.type = 'text/javascript';
+		wfscr.async = true;
+		wfscr.src = url + '&r=' + Math.random();
+		(document.getElementsByTagName('head')[0]||document.getElementsByTagName('body')[0]).appendChild(wfscr);
+		for (var i = 0; i < evts.length; i++) {
+			removeEvent(evts[i], logHuman);
+		}
+	};
+	for (var i = 0; i < evts.length; i++) {
+		addEvent(evts[i], logHuman);
+	}
+})('http://bestaircompressorr.com/?wordfence_logHuman=1&hid=3438BDA3A2A191FC4B47DAB0C595D87C');
+</script>			<style>
+				.jetpack-sso .message {
+					margin-top: 20px;
+				}
+
+				.jetpack-sso #login .message:first-child,
+				.jetpack-sso #login h1 + .message {
+					margin-top: 0;
+				}
+			</style>
+		<meta name="robots" content="noindex,follow"/>
+	</head>
+	<body class="login login-action-login wp-core-ui  locale-en-us jetpack-sso jetpack-sso-form-display">
+		<div id="login">
+		<h1><a href="https://wordpress.org/" title="Powered by WordPress" tabindex="-1">Best Air Compressors 2016 &#8211; Reviews &amp; Top Pick</a></h1>
+	
+<form name="loginform" id="loginform" action="http://bestaircompressorr.com/wp-login.php" method="post">
+	<p>
+		<label for="user_login">Username or Email<br/>
+		<input type="text" name="log" id="user_login" class="input" value="" size="20"/></label>
+	</p>
+	<p>
+		<label for="user_pass">Password<br/>
+		<input type="password" name="pwd" id="user_pass" class="input" value="" size="20"/></label>
+	</p>
+				<div style="margin: 5px 0 20px;">
+				<strong>Prove your humanity: </strong>
+				3 &nbsp; + &nbsp; 1 &nbsp; = &nbsp;
+				<input type="input" name="jetpack_protect_num" value="" size="2"/>
+				<input type="hidden" name="jetpack_protect_answer" value="d08d23f073a3e71c8cc70d3f81c4487a7ea33000"/>
+			</div>
+			<p class="forgetmenot"><label for="rememberme"><input name="rememberme" type="checkbox" id="rememberme" value="forever"/> Remember Me</label></p>
+	<p class="submit">
+		<input type="submit" name="wp-submit" id="wp-submit" class="button button-primary button-large" value="Log In"/>
+		<input type="hidden" name="redirect_to" value="http://bestaircompressorr.com/wp-admin/"/>
+		<input type="hidden" name="testcookie" value="1"/>
+	</p>
+</form>
+
+<p id="nav">
+	<a href="http://bestaircompressorr.com/wp-login.php?action=lostpassword">Lost your password?</a>
+</p>
+
+<script type="text/javascript">
+function wp_attempt_focus(){
+setTimeout( function(){ try{
+d = document.getElementById('user_login');
+d.focus();
+d.select();
+} catch(e){}
+}, 200);
+}
+
+wp_attempt_focus();
+if(typeof wpOnload=='function')wpOnload();
+</script>
+
+	<p id="backtoblog"><a href="http://bestaircompressorr.com/">&larr; Back to Best Air Compressors 2016 &#8211; Reviews &amp; Top Pick</a></p>
+	
+	</div>
+
+	
+	<link rel="stylesheet" id="jetpack_css-css" href="http://bestaircompressorr.com/wp-content/plugins/jetpack/css/jetpack.css" type="text/css" media="all"/>
+	<div class="clear"></div>
+	</body>
+	</html>
